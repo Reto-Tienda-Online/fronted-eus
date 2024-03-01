5f018ef3c26468feedcda4f82778d9e2756bf9b0
jokinnn <jo.gonzalez@aulanz.net>
2024-03-01 09:03:42 +0100

    First commit

---
+++ src/components/TrustPanel.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <div class="text-white mt-10 flex flex-row justify-evenly bg-gray-800 p-10">
+    <div
+      class="flex flex-row justify-center p-10 w-1/3 text-center border-r border-solid border-gray-400"
+    >
+      <font-awesome-icon icon="download" class="text-5xl" />
+      <div class="flex flex-col ml-5">
+        <h1 class="font-bold text-start">Super azkarra</h1>
+        <small class="text-start">Berehalako deskarga digitala</small>
+      </div>
+    </div>
+    <div
+      class="flex flex-row justify-center p-10 w-1/3 text-center border-r border-solid border-gray-400"
+    >
+      <font-awesome-icon icon="shield" class="text-5xl" />
+      <div class="flex flex-col ml-5">
+        <h1 class="font-bold text-start">Fidagarria eta segurua</h1>
+        <small class="text-start">10.000 jokotik gora</small>
+      </div>
+    </div>
+    <div class="flex flex-row justify-center p-10 w-1/3 text-center">
+      <font-awesome-icon icon="comment-dots" class="text-5xl" />
+      <div class="flex flex-col ml-5">+
+        <h1 class="font-bold text-start">Bezeroarentzako arreta</h1>
+        <small class="text-start">Agente erabilgarria 24/7</small>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<script setup>
+//Imprescindible "script setup"
+import Navbar from "../components/Navbar.vue";
+import Carousel from "../components/Carousel.vue";
+import Footer from "../components/Footer.vue";
+import ReviewPanel from "../components/ReviewPanel.vue";
+import Gallery from "../components/Gallery.vue";
+import Categories from "../components/Categories.vue";
+import TrustPanel from "../components/TrustPanel.vue";
+import CategoriaDetalle from "../components/CategoriaDetalle.vue";
+
+/*
+  TEMPLATE REAL
+
+  <CategoriaDetalle/>
+*/
+</script>
+
+<template>
+  <Navbar />
+  <div class="mt-10 text-center">
+    <h1 class="font-black text-4xl text-gray-200 font-barlow">
+      Online <span class="text-indigo-600">Joko </span>Denda
+    </h1>
+  </div>
+  <Carousel />
+  <div id="tendencias" class="mx-20">
+    <Gallery title="Joerak" />
+    <!--Lista de Juegos en Tendencia-->
+  </div>
+  <TrustPanel />
+  <div id="reservas" class="mx-20">
+    <Gallery title="Erreserbak"/>
+    <!--Lista de Juegos Coming Soon-->
+  </div>
+  <ReviewPanel />
+  <!--Pasar lista de comentarios positivos y coger uno random-->
+  <!--Se podría poner mas comentarios pero esta en veremos-->
+  <div id="bestSellers" class="mx-20">
+    <Gallery title="Salduenak" />
+  </div>
+  <!--Colocar algo en medio aqui-->
+  <div id="categories" class="mx-20">
+    <Categories />
+  </div>
+  <Footer />
+</template>
