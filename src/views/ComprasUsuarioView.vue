7d3f3261c75959fda822de19103939c998c982b9
jokinnn <jo.gonzalez@aulanz.net>
2024-03-04 12:08:34 +0100

    Footers added

--- src/views/ComprasUsuarioView.vue
+++ src/views/ComprasUsuarioView.vue
@@ -1,8 +1,10 @@
 <template >
     <Navbar />
     <ComprasUsuario />
+    <Footer/>
 </template>
 <script setup>
     import Navbar from '@components/Navbar.vue';
     import ComprasUsuario from '@components/ComprasUsuario.vue';
+    import Footer from '../components/Footer.vue';
 </script>
--- src/views/PagoView.vue
+++ src/views/PagoView.vue
@@ -2,6 +2,7 @@
     <div>
         <NavBarCesta activeElement="Pago"/>
         <PagoMain />
+        <Footer/>
     </div>
 </template>
 
@@ -9,5 +10,6 @@
 //Imprescindible "script setup"
 import NavBarCesta from '@components/NavBarCesta.vue'
 import PagoMain from '@components/PagoMain.vue'
+import Footer from '../components/Footer.vue';
 </script>
 
